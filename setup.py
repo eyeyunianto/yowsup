9a35716062ade0fe7d14388506749bcf25d1a805
Matthias Gehre <M.Gehre@gmx.de>
2013-03-10 12:20:00 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from pkgutil import walk_packages
+
+setup(name='yowsup',
+      version='1.0',
+      packages=['Yowsup', 'Yowsup.Auth', 'Yowsup.Common', 'Yowsup.ConnectionIO', 'Yowsup.Contacts', 'Yowsup.Interfaces', 'Yowsup.Registration',
+                 'Yowsup.Interfaces.DBus', 'Yowsup.Interfaces.Lib', 'Yowsup.Auth.mechanisms', 'Yowsup.Common.Http',
+                 'Yowsup.Registration.v1', 'Yowsup.Registration.v2'],
+      package_dir = {'': 'src'},
+      data_files = [
+                   ('/usr/share/yowsup',['src/countries.csv']),
+                   ('/usr/share/dbus-1/services/', ['com.yowsup.methods.service']) ],
+      )
